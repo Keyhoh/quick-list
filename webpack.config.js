da74f9f9b1bc817b83af68cbaa938c70398d10e3
keyhoh <iheyhoh+github@gmail.com>
2020-01-02 20:25:00 +0900

    ビルド環境を構築

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+
+module.exports = {
+  mode: "development",
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  module: {
+    rules: [{
+      test: /\.tsx?$/,
+      use: 'ts-loader',
+      exclude: /node_modules/,
+    },],
+  },
+  resolve: {
+    extensions: ['tsx', 'ts']
+  },
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist'),
+  },
+};
