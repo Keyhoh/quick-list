d0ab35a3dc38b2f1becbe676fe245ccbabf50488
Keyhoh <iheyhoh@gmail.com>
2020-01-05 14:54:01 +0900

    todo-checkを実装

--- src/operations/Todo.spec.ts
+++ src/operations/Todo.spec.ts
@@ -6,33 +6,33 @@ import EmptyNameError from "./Error/EmptyNameError";
 describe('Test for todo-id', (): void => {
     test('The value type of todo-id is UUID.', (): void => {
         const todo = new Todo('Todo Name');
-        expect(UUID.test(todo.id)).toBeTruthy();
+        expect(UUID.test(todo.id)).toBe(true);
     });
 });
 
 describe('Test for todo-name', (): void => {
     test('It is able to give todo a name.', (): void => {
         const TODO_NAME = 'Todo Name';
-        expect((): Todo => new Todo(TODO_NAME)).not.toThrow();
+        expect((): void => void new Todo(TODO_NAME)).not.toThrow();
         expect(new Todo(TODO_NAME).name).toEqual(TODO_NAME);
     });
 
     test('The max length of todo-name is 32.', (): void => {
         const JUST_LENGTH_NAME = 'この文字列　は３２文字　丁度で構成　されていま　す。文字数　稼ぎ';
         expect(JUST_LENGTH_NAME).toHaveLength(32);
-        expect((): Todo => new Todo(JUST_LENGTH_NAME)).not.toThrow();
+        expect((): void => void new Todo(JUST_LENGTH_NAME)).not.toThrow();
         const OVER_LENGTH_NAME = 'この文字列　は３３文字　丁度で構成　されていま　す。文字数　稼ぎ。';
         expect(OVER_LENGTH_NAME).toHaveLength(33);
-        expect((): Todo => new Todo(OVER_LENGTH_NAME)).toThrow(TooLongNameError);
+        expect((): void => void new Todo(OVER_LENGTH_NAME)).toThrow(TooLongNameError);
     });
 
     test('The minimum length of todo-name is 1.', (): void => {
         const EMPTY_NAME = '';
         expect(EMPTY_NAME).toHaveLength(0);
-        expect((): Todo => new Todo(EMPTY_NAME)).toThrow(EmptyNameError);
+        expect((): void => void new Todo(EMPTY_NAME)).toThrow(EmptyNameError);
         const MINIMUM_NAME = 'あ';
         expect(MINIMUM_NAME).toHaveLength(1);
-        expect((): Todo => new Todo(MINIMUM_NAME)).not.toThrow();
+        expect((): void => void new Todo(MINIMUM_NAME)).not.toThrow();
     });
 
     test('It is able to update todo-name.', (): void => {
@@ -42,5 +42,28 @@ describe('Test for todo-name', (): void => {
         const NEXT_NAME = 'Next Name';
         todo.name = NEXT_NAME;
         expect(todo.name).toBe(NEXT_NAME);
+    });
+});
+
+describe('Test for todo-check', (): void => {
+    let todo: Todo;
+    beforeEach((): void => {
+        todo = new Todo('Todo Name');
+    });
+
+    test('Todo is not checked at created.', (): void => {
+        expect(todo.isChecked).toBe(false);
+    });
+
+    test('It is able to check todo.', (): void => {
+        expect((): void => todo.check()).not.toThrow();
+        expect(todo.isChecked).toBe(true);
+    });
+
+    test('It is able to uncheck todo.', (): void => {
+        todo.check();
+        expect(todo.isChecked).toBe(true);
+        expect((): void => todo.uncheck()).not.toThrow();
+        expect(todo.isChecked).toBe(false);
     });
 });
--- src/operations/Todo/Todo.ts
+++ src/operations/Todo/Todo.ts
@@ -4,21 +4,34 @@ import Name from "./Name";
 export default class Todo {
     private readonly _id: Id;
     private _name: Name;
+    private _isChecked: boolean = false;
 
-    constructor(name: string) {
+    public constructor(name: string) {
         this._id = new Id();
         this._name = new Name(name);
     }
 
-    get id(): string {
+    public get id(): string {
         return this._id.value;
     }
 
-    get name(): string {
+    public get name(): string {
         return this._name.value;
     }
 
-    set name(name: string) {
+    public set name(name: string) {
         this._name = new Name((name));
+    }
+
+    public get isChecked(): boolean {
+        return this._isChecked;
+    }
+
+    public check(): void {
+        this._isChecked = true;
+    }
+
+    public uncheck(): void {
+        this._isChecked = false;
     }
 }
