9284bea9a59ad30a0b79285bd47e77bdf482bf5c
keyhoh <iheyhoh+github@gmail.com>
2020-01-07 20:00:25 +0900

    空配列もtodo-list-contentsに設定できる

--- src/operations/TodoList.spec.ts
+++ src/operations/TodoList.spec.ts
@@ -3,8 +3,14 @@ import Todo from "./Todo";
 
 describe('Test for todo-list-contents', (): void => {
     test('It is able to set todo-list the list of todo.', (): void => {
-        const LIST_OF_TODO = Array(10).map((i: number): Todo => new Todo(`Todo-${i}`));
+        const LIST_OF_TODO: Todo[] = Array.from({length: 10}, (_, i: number): Todo => new Todo(`Todo-${i}`));
         expect((): void => void new TodoList(LIST_OF_TODO)).not.toThrow();
         expect(new TodoList(LIST_OF_TODO).contents).toEqual(LIST_OF_TODO);
+    });
+
+    test('It is able to set todo-list empty-list.', (): void => {
+        const EMPTY_LIST: Todo[] = [];
+        expect((): void => void new TodoList(EMPTY_LIST)).not.toThrow();
+        expect(new TodoList([]).contents).toEqual([]);
     });
 });
