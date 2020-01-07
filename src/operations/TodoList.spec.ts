b1b3b298a9b650209a23f5321e0d795234f57f2a
keyhoh <iheyhoh+github@gmail.com>
2020-01-07 19:36:37 +0900

    test名を修正

--- src/operations/TodoList.spec.ts
+++ src/operations/TodoList.spec.ts
@@ -1,7 +1,7 @@
 import TodoList from "./TodoList";
 import Todo from "./Todo";
 
-describe('Test for todo-list-id', (): void => {
+describe('Test for todo-list-contents', (): void => {
     test('It is able to set todo-list the list of todo.', (): void => {
         const LIST_OF_TODO = Array(10).map((i: number): Todo => new Todo(`Todo-${i}`));
         expect((): void => void new TodoList(LIST_OF_TODO)).not.toThrow();
