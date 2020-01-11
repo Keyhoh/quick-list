bdf117851ef648e144d74502fbb0930c43bb9f93
keyhoh <iheyhoh+github@gmail.com>
2020-01-11 23:28:38 +0900

    todo-list内の次のtodoを取得するnextを実装

--- src/operations/TodoList.spec.ts
+++ src/operations/TodoList.spec.ts
@@ -25,9 +25,27 @@ describe('Test for focus', (): void => {
         expect(todoList.current).toBe(LIST_OF_TODO[0]);
     });
 
-    test('The current-todo is nothing if todo-list-contents is empty.', (): void => {
+    test('Throw error if todo-list-contents is empty.', (): void => {
         const todoList = new TodoList([]);
         expect(todoList.contents.length).toBe(0);
         expect((): void => void todoList.current).toThrow(NoSuchTodoError);
+    });
+
+    test('It is able to get the next todo.', (): void => {
+        const LIST_OF_TODO: Todo[] = Array.from({length: 10}, (_, i: number): Todo => new Todo(`Todo-${i}`));
+        const todoList = new TodoList(LIST_OF_TODO);
+        expect(todoList.contents.length > 1).toBe(true);
+        expect(todoList.current).toBe(LIST_OF_TODO[0]);
+        expect(todoList.next()).toBe(LIST_OF_TODO[1]);
+        expect(todoList.current).toBe(LIST_OF_TODO[1]);
+    });
+
+    test('Throw error if there is no next todo.', (): void => {
+        const todo = new Todo('Todo Name');
+        const todoList = new TodoList([todo]);
+        expect(todoList.contents.length).toBe(1);
+        expect((): void => void todoList.next()).toThrow(NoSuchTodoError);
+        expect((): void => void todoList.current).not.toThrow();
+        expect(todoList.current).toBe(todo);
     });
 });
--- src/operations/TodoList/TodoList.ts
+++ src/operations/TodoList/TodoList.ts
@@ -3,11 +3,11 @@ import NoSuchTodoError from "../Error/NoSuchTodoError";
 
 export default class TodoList {
     private readonly _contents: Todo[];
-    private readonly _current: number;
+    private _current: number;
 
     constructor(contents: Todo[]) {
         this._contents = contents;
-        this._current = contents.length === 0 ? -1 : 0;
+        this._current = contents.length > 0 ? 0 : -1;
     }
 
     get contents(): Todo[] {
@@ -15,7 +15,13 @@ export default class TodoList {
     }
 
     get current(): Todo {
-        if(this.contents.length === 0) throw new NoSuchTodoError();
+        if (this.contents.length === 0) throw new NoSuchTodoError();
         return this._contents[this._current];
+    }
+
+    public next(): Todo {
+        if (this.contents.length - this._current <= 1) throw new NoSuchTodoError();
+        this._current++;
+        return this.current;
     }
 }
