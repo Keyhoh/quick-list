c4bb1bce2c946257095b5e0fc0fbe3319ae5c133
keyhoh <iheyhoh+github@gmail.com>
2020-02-03 00:08:23 +0900

    動作確認用のコードを修正

--- src/app/NormalMode/reducers.ts
+++ src/app/NormalMode/reducers.ts
@@ -5,8 +5,7 @@ export interface State {
     list: Todo[],
 }
 
-const TODO_LIST: Todo[] = Array.from({length: 10}, (_, i: number) => new Todo(`Todo-${i}`));
-let initState: State = {list: TODO_LIST};
+let initState: State = {list: []};
 
 export const Reducer = (state: State = initState, action: TodoActionTypes): State => {
     switch (action.type) {
