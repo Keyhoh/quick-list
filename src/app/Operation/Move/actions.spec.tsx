7913a490fe9874747886c8d85758a028e1809316
keyhoh <iheyhoh+github@gmail.com>
2020-02-24 16:44:20 +0900

    Moveのテストをreducerとactionのテストに分割

---
+++ src/app/Operation/Move/actions.spec.tsx
@@ -0,0 +1,32 @@
+import {DOWN, down, UP, up} from "./index";
+import {State} from "../../State";
+import {getTodoList} from "../../../__tests__/util";
+import Mode from "../../../mode";
+
+import configureStore, {MockStoreCreator, MockStoreEnhanced} from 'redux-mock-store';
+import {Kind} from "../../Action";
+
+const mockStore: MockStoreCreator<State> = configureStore<State>([]);
+
+describe('Test for App', (): void => {
+    let store: MockStoreEnhanced<State>;
+
+    const TIMES: number = 10;
+
+    const InitState: State = {
+        mode: Mode.NORMAL,
+        contents: getTodoList(TIMES),
+        current: 0,
+    };
+
+    beforeEach((): void => void (store = mockStore(InitState)));
+
+    test('It is called UP and DOWN action.', (): void => {
+        store.dispatch(up());
+        store.dispatch(down());
+        expect(store.getActions()).toEqual([
+            {kind: Kind.MOVE, type: UP},
+            {kind: Kind.MOVE, type: DOWN},
+        ]);
+    });
+});
