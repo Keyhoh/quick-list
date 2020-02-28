75656c23e07e637afecd371bbdd1ff975a58b6a8
keyhoh <iheyhoh+github@gmail.com>
2020-02-29 00:21:54 +0900

    reducerのテストを追加

--- src/app/Reducer/index.ts
+++ src/app/Reducer/index.ts
@@ -1,17 +1,17 @@
 import {InitState, State} from "../State";
-import {IAction, Kind} from "../Action";
+import {Kind} from "../Action";
 import {ManufactureActionTypes, ManufactureReducer} from "../Operation/Manufacture";
 import {MoveActionTypes, MoveReducer} from "../Operation/Move";
 import {ModeActionTypes, ModeReducer} from "../Operation/Mode";
 
-export default function (state: State = InitState, action: IAction): State {
+export default function (state: State = InitState, action: ModeActionTypes | MoveActionTypes | ManufactureActionTypes): State {
     switch (action.kind) {
         case Kind.MODE:
-            return ModeReducer(state, action as ModeActionTypes);
+            return ModeReducer(state, action);
         case Kind.MOVE:
-            return MoveReducer(state, action as MoveActionTypes);
+            return MoveReducer(state, action);
         case Kind.MANUFACTURE:
-            return ManufactureReducer(state, action as ManufactureActionTypes);
+            return ManufactureReducer(state, action);
     }
     return InitState;
 };
